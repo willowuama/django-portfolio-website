16513d672697795b33aff2814915cdeee3ae17bd
WillOwuama <willowuama@gmail.com>
2018-09-09 13:38:53 -0500

    Deleted blog app

--- blog/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Blog(models.Model):
-	image = models.ImageField(upload_to="images/")
-	title = models.CharField(max_length=255)
-	pub_date = models.DateTimeField()
-	body = models.TextField()
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jobs.apps.JobsConfig',
-    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio/static'),
+
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
