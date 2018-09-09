bcdeb8a97efc1b5beb410680d49ec2b9f429095a
WillOwuama <willowuama@gmail.com>
2018-09-09 17:08:26 -0500

    Turned debug mode off

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%%o6l(70rs0u0gr-vst$4ok!$(%6s3=#$w89zfyidig_504d_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
